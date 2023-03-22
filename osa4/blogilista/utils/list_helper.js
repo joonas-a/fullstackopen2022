8c3a18fe76252cc7a2d27b23e669821c518f70bc
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2023-03-22 13:13:12 +0200

    4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -9,4 +9,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
-module.exports = { dummy, totalLikes }
+// return 0 if no blogs
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  const reducer = (mostLiked, item) => {
+    return mostLiked.likes > item.likes ? mostLiked : item
+  }
+  return blogs.reduce(reducer, blogs[0])
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
