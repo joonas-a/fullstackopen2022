1dcb7e2a2efa450076d3daa1bc1e92be8561a803
Joonas Ahovalli <joonas.ahovalli@gmail.com>
2023-08-03 14:50:43 +0300

    5.3

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -8,12 +8,17 @@ const App = () => {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [user, setUser] = useState(null)
+  const [blogTitle, setBlogTitle] = useState("")
+  const [blogAuthor, setBlogAuthor] = useState("")
+  const [blogUrl, setBlogUrl] = useState("")
+  const [blogLikes, setBlogLikes] = useState("")
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem("currentlyLoggedUser")
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -31,12 +36,14 @@ const App = () => {
       })
 
       window.localStorage.setItem("currentlyLoggedUser", JSON.stringify(user))
+      blogService.setToken(user.token)
       setUser(user)
       setUsername("")
       setPassword("")
     } catch (exception) {
       setUsername("")
       setPassword("")
+      setUser(null)
       console.log("Invalid credentials")
     }
   }
@@ -46,43 +53,96 @@ const App = () => {
     setUser(null)
   }
 
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-            <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-            <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    )
+  const handleNewBlog = (event) => {
+    event.preventDefault()
+    const newBlog = {
+      title: blogTitle,
+      author: blogAuthor,
+      url: blogUrl,
+      likes: blogLikes,
+    }
+    blogService.createNew(newBlog)
   }
 
-  return (
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const mainView = () => (
     <div>
       <h2>Blogs</h2>
       Logged in as {user.username}
       <button onClick={logOut}>Log out</button>
+      <form onSubmit={handleNewBlog}>
+        <h3>Add a new blog</h3>
+        <div>
+          Title:
+          <input
+            type="text"
+            value={blogTitle}
+            name="Title"
+            onChange={({ target }) => setBlogTitle(target.value)}
+          />
+        </div>
+        <div>
+          Author:
+          <input
+            type="text"
+            value={blogAuthor}
+            name="Author"
+            onChange={({ target }) => setBlogAuthor(target.value)}
+          />
+        </div>
+        <div>
+          Url:
+          <input
+            type="text"
+            value={blogUrl}
+            name="Url"
+            onChange={({ target }) => setBlogUrl(target.value)}
+          />
+        </div>
+        <div>
+          Likes:
+          <input
+            type="text"
+            value={blogLikes}
+            name="Likes"
+            onChange={({ target }) => setBlogLikes(target.value)}
+          />
+        </div>
+        <button type="submit">Add new blog</button>
+      </form>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
+    </div>
+  )
+
+  return (
+    <div>
+      <h1>Blog App</h1>
+      {!user ? loginForm() : mainView()}
     </div>
   )
 }
