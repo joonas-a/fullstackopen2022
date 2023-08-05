515a6b4301f57b20c751efe1548968b4521076a6
Joonas Ahovalli <joonas.ahovalli@gmail.com>
2023-08-05 13:26:47 +0300

    5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const handleLike = (event) => {
+    event.stopPropagation()
+    console.log("Click!")
+  }
+
+  return (
+    <div onClick={() => setExpanded(!expanded)} className="blog">
+      {!expanded && (
+        <div>
+          {blog.title} by {blog.author}
+        </div>
+      )}
+      {expanded && (
+        <div>
+          {blog.title} by {blog.author}
+          <br />
+          Url: {blog.url}
+          <br />
+          Likes: {blog.likes}{" "}
+          <button onClick={(event) => handleLike(event)}>Like</button>
+          <br />
+          Added by: {blog.user.name}
+          <br />
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
