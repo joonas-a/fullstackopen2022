4bb99561f62641e66c665a72424baa43cc80234b
Joonas Ahovalli <joonas.ahovalli@gmail.com>
2023-08-11 12:52:38 +0300

    5.19

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -30,4 +30,30 @@ describe('Blog app', function () {
       cy.contains('Invalid username or password')
     })
   })
+
+  describe('When logged in', function () {
+    beforeEach(function () {
+      cy.request('POST', 'http://localhost:3003/api/login', {
+        username: 'root',
+        password: 'root123',
+      }).then((response) => {
+        localStorage.setItem(
+          'currentlyLoggedUser',
+          JSON.stringify(response.body)
+        )
+        cy.visit('http://localhost:3000')
+      })
+    })
+
+    it('A new blog can be created', function () {
+      cy.contains('New blog').click()
+      cy.get('#title-input').type('Cypress Testing')
+      cy.get('#author-input').type('Testuser')
+      cy.get('#url-input').type('http://localhost:3000')
+      cy.contains('Add new blog').click()
+
+      cy.contains('Added a new blog: Cypress Testing by Testuser')
+      cy.get('.blog').contains('Cypress Testing by Testuser')
+    })
+  })
 })
