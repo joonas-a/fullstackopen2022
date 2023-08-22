f63d18a8945d3527d7eb6deacad4ea4dd82467aa
Joonas Ahovalli <joonas.ahovalli@gmail.com>
2023-08-22 11:22:16 +0300

    6.22

--- osa6/query-anecdotes/src/requests.js
+++ osa6/query-anecdotes/src/requests.js
@@ -6,3 +6,8 @@ export const getAnecdotes = () => axios.get(baseUrl).then((res) => res.data)
 
 export const createAnecdote = (content) =>
   axios.post(baseUrl, content).then((res) => res.data)
+
+export const updateAnecdote = (updatedAnecdote) =>
+  axios
+    .put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote)
+    .then((res) => res.data)
