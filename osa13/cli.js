df58a6f48523ddec36e6b4f79393a7cb72a0fc5d
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2023-10-16 13:41:43 +0300

    13.1 - 13.3

---
+++ osa13/cli.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      default: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
+  }
+);
+
+Blog.findAll().then((blogs) =>
+  blogs.forEach((blog) =>
+    console.log(
+      `${blog.dataValues.author}: ${blog.dataValues.title}, ${blog.dataValues.likes} likes`
+    )
+  )
+);
---
+++ osa13/commands.sql
@@ -0,0 +1,15 @@
+CREATE TABLE blogs (
+  id SERIAL PRIMARY KEY,
+  author TEXT,
+  url TEXT NOT NULL,
+  title TEXT NOT  NULL,
+  likes INTEGER DEFAULT 0,
+);
+
+INSERT INTO blogs (author, url, title) VALUES (
+  'Dan Abramov', 'https://overreacted.io/writing-resilient-components/', 'Writing Resilient Components'
+);
+
+INSERT INTO blogs (author, url, title) VALUES (
+  'Martin Fowler', 'https://martinfowler.com/articles/is-quality-worth-cost.html', 'Is High Quality Software Worth the Cost?'
+);
