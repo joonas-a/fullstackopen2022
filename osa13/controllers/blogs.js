f38609066f9ddd753c7bc875ecdfd1c9e0d4460c
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2023-10-24 18:13:44 +0300

    13.14

--- osa13/controllers/blogs.js
+++ osa13/controllers/blogs.js
@@ -6,10 +6,20 @@ const { Blog, User } = require('../models');
 
 router.get('/', async (req, res) => {
   const where = {};
+
   if (req.query.search) {
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`,
-    };
+    where[Op.or] = [
+      {
+        title: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
+      },
+      {
+        author: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
+      },
+    ];
   }
   const allblogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
